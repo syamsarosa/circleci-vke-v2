a55a345243fd0cae418738c59020e1c435d87202
Syam Sarosa <syamsarosa@gmail.com>
2025-09-24 14:48:22 +0000

    Test circleci env

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
   <h1><?php echo "Hello from Vultr"; ?></h1>
   <h1><?php echo "Test env circleci"; ?></h1>
   <h1><?php echo "Test env circleci 2"; ?></h1>
+  <h1><?php echo "Test env circleci 3" ?></h1>
 </body>
 </html>
